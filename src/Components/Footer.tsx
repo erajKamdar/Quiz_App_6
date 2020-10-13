cc05ba53dcaefdf7d5c8bc8828c20646827b3cc7
Eraj <erajkamdar15@gmail.com>
2020-10-13 15:48:39 +0500

    Initial Commit

---
+++ src/Components/Footer.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import "./../App.css";
+import {FaGithub} from "react-icons/fa";
+import { format } from "util";
+
+function Footer() {
+  return (
+    <div className="footer-container">
+      <div className="page-wrapper">
+        
+      </div>
+      
+
+      <footer>
+        <div className="footer ">
+          
+            <div className="footer-section about">
+              <div className="logo-text"> &copy; ERAJ HANIF
+              <a
+                  href="https://github.com/erajKamdar"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                  className='setGit'
+                >
+                 <FaGithub></FaGithub>
+                </a>
+              </div>
+        
+          </div>
+          {/* <div className="footer-bottom">
+          </div> */}
+        </div>
+      </footer>
+    </div>
+  );
+}
+
+export default Footer;
